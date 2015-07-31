4f2b70a75927b2377e1865ffa1af9ee198e2ecb9
Jack Chang <wei0831@gmail.com>
2015-07-30 20:08:42 -0700

    add server port ip option

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,8 @@
 module.exports = function(){
-
+  var manual = true;
   return {
-    server_port: 4000,
-    server_IP: "127.0.0.1",
+    server_port: manual ? 4000 : process.env.PORT,
+    server_IP: manual ? "127.0.0.1" : process.env.IP,
     user: "admin:root",
     url: "ds063869.mongolab.com:63869",
     dbname: "bonapetit",
